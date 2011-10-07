025b1c6b32ad9c0c76854db750211f2609fc9529
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-10-07 15:03:17 -0700

    Rails 3.0.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestOmniDevise::Application.config.secret_token = '57729458fde4ab453b6629f43b6ce1c91f256a5a0d81b3e34b8ab2e523f2e28ccbae78f8d160a7d1eb1f889a08a6039023c04bf845862d40fbe6505b98e4668d'
